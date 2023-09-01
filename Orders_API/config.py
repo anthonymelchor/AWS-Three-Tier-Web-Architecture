6038088a6addb69c1ed47744f4f73c5f4615e5b1
Anthony Melchor <anthonymelchor03@gmail.com>
2023-09-01 11:56:47 -0500

    adding a gitignore file

--- Orders_API/config.py
+++ Orders_API/config.py
@@ -1,5 +1,9 @@
 # config.py
 class Configuration:
     def __init__(self):
-        self.db_uri = "mysql://root:root@localhost/inventory"
+        self.db_user = ""
+        self.db_password = ""
+        self.db_host = ""
+        self.db_name = ""
+        self.db_uri = f"mysql://{self.db_user}:{self.db_password}@{self.db_host}/{self.db_name}"
 
